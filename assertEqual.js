6e5d32e65bba51bd0b44a142a5d52772bf459d57
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-07 17:16:06 -0500

    Colourful code tester

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+    return 'Assertion Passed: ' + actual + ' === ' + expected;
+  } else {
+    console.log('🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected);
+    return 'Assertion Failed: ' + actual + ' !== ' + expected;
+  }
+};
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual('hello', 'hello');
