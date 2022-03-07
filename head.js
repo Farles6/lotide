d8e174e19e647ca463c1d20fba52b541289c4c33
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-07 18:40:36 -0500

    takes first element of array and returns it

---
+++ head.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return `✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return `🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+/*
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual('hello', 'hello');
+*/
+
+const head = function(actual, expected) {
+ return actual[0];
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 1);
+assertEqual(head([6]), 5);
