4b0b81547794d9860644ecb6b159c451fa0ff411
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-08 19:14:20 -0500

    assert function that compares arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,16 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    console.log(`🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+    return;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+      return;
+    }
+  }
+  console.log(`✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
