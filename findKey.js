d76013fe2433677db94db6f13b6db31b284dab46
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-10 18:38:08 -0500

    formatting

--- findKey.js
+++ findKey.js
@@ -9,22 +9,24 @@ const assertEqual = (actual, expected) => {
 };
 
 const findKey = (obj, cb) => {
-  for (let key in obj){
-    if (cb(obj[key])){
-    console.log(key)
-  return key;
-    
-  } 
-}
-}
+  for (let key in obj) {
+    if (cb(obj[key])) {
+      console.log(key);
+      return key;
+    }
+  }
+};
 
-const result = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+const result = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
 
-assertEqual((result), 'noma')
+assertEqual(result, "noma");
