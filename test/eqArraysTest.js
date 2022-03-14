1d7bce88db27f9125c2e955707eb583f8e8092d0
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-14 14:40:49 -0400

    added a test file, made modular and refactored

---
+++ test/eqArraysTest.js
@@ -0,0 +1,13 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(['hello', 'man'], ['hello', 'man']), true);
+assertEqual(eqArrays(['hello', 'man'], ['hello', 'man', 'goodbye']), true);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
